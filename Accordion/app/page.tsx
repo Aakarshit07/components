edefe89cf8c72d6d6a945f1291e9bf2d8f2a4be6
aakarshit choudhary <aakarshit070@gmail.com>
2026-03-01 20:56:04 +0530

    feat: support multiple open accordions

---
+++ Accordion/app/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+import { useState } from 'react';
+import Accordion from './accordion';
+import { open } from 'fs/promises';
+
+export default function Home() {
+  const [openIds, setOpenIds] = useState<number[]>([]);
+  const section = [
+    {
+      head: 'This is the title of the accordion',
+      id: 1,
+      content: ` Hello Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    Necessitatibus, explicabo vitae architecto a, debitis illo perferendis
+    laborum beatae dolorum minima tempora odit. In.`,
+    },
+    {
+      head: 'This is the title of the accordion',
+      id: 3,
+      content: ` Hello Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    Necessitatibus, explicabo vitae architecto a, debitis illo perferendis
+    laborum beatae dolorum minima tempora odit. In.`,
+    },
+    {
+      head: 'This is the title of the accordion',
+      id: 2,
+      content: ` Hello Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    Necessitatibus, explicabo vitae architecto a, debitis illo perferendis
+    laborum beatae dolorum minima tempora odit. In.`,
+    },
+  ];
+
+  const handleOpen = (id: number) => {
+    if (openIds.includes(id)) {
+      setOpenIds(openIds.filter((item) => item != id));
+    } else {
+      setOpenIds([...openIds, id]);
+    }
+  };
+
+  return (
+    <main className="min-w-screen min-h-screen p-2">
+      <h1>Accordion Demo</h1>
+      <br />
+      {section &&
+        section.map((item) => (
+          <Accordion
+            header={<div>{item.head}</div>}
+            onOpen={() => handleOpen(item.id)}
+            isOpen={openIds.includes(item.id)}
+            key={item.id}
+          >
+            <div>{item.content}</div>
+          </Accordion>
+        ))}
+    </main>
+  );
+}
